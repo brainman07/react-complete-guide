a3dd8d9dc18e9a666c141bb41eaf7059915cf955
brainman07 <weaselwiz7@gmail.com>
2019-02-05 00:16:49 +0200

    Person.css and react inline style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         persons: [
             {name: 'Max', age: 28},
             {name: 'Manu', age: 29},
-            {name: 'Stephanie', age: 26}
+            {name: 'Irina', age: 26}
         ]
     }
 
@@ -29,11 +29,24 @@ class App extends Component {
     }
 
     render() {
+        const buttonStyle = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        }
+
         return (
             <div className="App">
                 <h1>Hi, i'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+                <button 
+                    style={buttonStyle}
+                    onClick={() => this.switchNameHandler('Maximilian!!')}
+                >
+                    Switch Name
+                </button>
                 <Person 
                     name={this.state.persons[0].name} 
                     age={this.state.persons[0].age}
