b277662d4427fe5dcdc96da53411856aadbb7217
brainman07 <weaselwiz7@gmail.com>
2019-02-04 22:36:06 +0200

    nameChangeHandler & inputs in Person comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 26}
+        ]
+    }
+
+    switchNameHandler = (newName) => {
+        // console.log('Was clicked!');
+        this.setState({persons: [
+            {name: newName, age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 27}
+        ]});
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({persons: [
+            {name: 'Max', age: 28},
+            {name: event.target.value, age: 29},
+            {name: 'Stephanie', age: 26}
+        ]});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, i'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+                <Person 
+                    name={this.state.persons[0].name} 
+                    age={this.state.persons[0].age}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Max!!!')}
+                    changed={this.nameChangeHandler}
+                >My hobbies: Racing
+                </Person>
+                <Person 
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, i\'m a React App'));
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}> I'm {props.name} and I am {props.age} years old! </p>
+            <p>{props.children}</p>
+            <input 
+                type="text"
+                value={props.name}
+                onChange={props.changed}
+            />
+        </div>
+    )
+}
+
+export default person;
